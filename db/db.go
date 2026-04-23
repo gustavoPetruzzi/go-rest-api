edb27b22d9986d732a0e80f14e0a9d546e5fc1be
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2026-04-22 23:54:28 -0300

    feat: add registration table

--- db/db.go
+++ db/db.go
@@ -54,4 +54,20 @@ func createTables() {
 	if err != nil {
 		panic("Could not create events table")
 	}
+
+	createRegistrationTable := `
+	CREATE TABLE IF NOT EXISTS registrations (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER,
+		event_id INTEGER,
+		FOREIGN KEY(user_id) REFERENCES users(id),
+		FOREIGN KEY(event_id) REFERENCES events(id)
+	)
+	`
+
+	_, err = DB.Exec(createRegistrationTable)
+
+	if err != nil {
+		panic("Could not create registrations table")
+	}
 }
